4c7ddfddfe76e18f6f301548587c222bdef2539b
hasiotis <hasiotis@7a9393eb-3c4c-0410-9195-e5c8997374a0>
2008-05-22 19:42:29 +0000

    Adding dialogs to the tool
    git-svn-id: http://openeafdss.googlecode.com/svn/trunk@6 7a9393eb-3c4c-0410-9195-e5c8997374a0

--- OpenEAFDSS.pl
+++ OpenEAFDSS.pl
@@ -18,14 +18,13 @@ my($menuFile) = [
 ];
 my($menuActions) = [
 	{ -label => 'Get Status        ^U', -value => \&getStatusDialog  },
-	{ -label => 'Set Headers       ^H', -value => \&setHeadersDialog  },
-	{ -label => 'Get Headers       ^E', -value => \&getHeadersDialog  },
+	{ -label => 'Set Headers       ^E', -value => \&setHeadersDialog  },
+	{ -label => 'Get Headers       ^R', -value => \&getHeadersDialog  },
 	{ -label => 'Read Time         ^T', -value => \&readTimeDialog  },
 	{ -label => 'Read Device ID    ^I', -value => \&readDeviceIdDialog  },
 	{ -label => 'Version Info      ^V', -value => \&versionInfoDialog  },
-	{ -label => 'Display Message   ^M', -value => \&displayMessageDialog  },
+	{ -label => 'Display Message   ^G', -value => \&displayMessageDialog  },
 ];
-
 my($menuBar) = [
 	{ -label => 'File',    -submenu => $menuFile },
 	{ -label => 'Actions', -submenu => $menuActions }
@@ -44,16 +43,16 @@ my($status) = $statusBar->add(
 	-bg             => 'blue',
 );
 
-
-$cui->set_binding(sub {$menu->focus()},   "\cX");
-$cui->set_binding( \&settingsDialog,      "\cS");
-$cui->set_binding( \&exitDialog,          "\cQ");
-$cui->set_binding( \&getHeadersDialog,    "\cH");
-$cui->set_binding( \&setHeadersDialog,    "\cE");
-$cui->set_binding( \&readTimeDialog ,     "\cT");
-$cui->set_binding( \&readDeviceIdDialog , "\cI");
-$cui->set_binding( \&versionInfoDialog ,  "\cV");
-$cui->set_binding( \&displayImageDialog , "\cM");
+$cui->set_binding(sub {$menu->focus()},     "\cX");
+$cui->set_binding( \&settingsDialog,        "\cS");
+$cui->set_binding( \&exitDialog,            "\cQ");
+$cui->set_binding( \&getStatusDialog,       "\cU");
+$cui->set_binding( \&setHeadersDialog,      "\cE");
+$cui->set_binding( \&getHeadersDialog,      "\cR");
+$cui->set_binding( \&readTimeDialog ,       "\cT");
+$cui->set_binding( \&readDeviceIdDialog ,   "\cI");
+$cui->set_binding( \&versionInfoDialog ,    "\cV");
+$cui->set_binding( \&displayMessageDialog , "\cG");
 
 $cui->mainloop();
 
@@ -72,9 +71,72 @@ sub exitDialog {
 	exit(0) if $return;
 }
 
+sub settingsDialog {
+	$cui->dialog(
+		-title => "Device Settings",
+		-message => "HALLO"
+	);
+}
+
 sub getStatusDialog {
 	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
 	%reply = $FD->GetStatus();
-	$cui->dialog("Raw Status: " . $reply{DATA});
+	$cui->dialog(
+		-title => "Device Status",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
 }
 
+sub setHeadersDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->SetHeader();
+	$cui->dialog(
+		-title => "Set Headers",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
+
+sub getHeadersDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->GetHeader();
+	$cui->dialog(
+		-title => "Get Headers",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
+
+sub readTimeDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->ReadTime();
+	$cui->dialog(
+		-title => "Device Time",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
+
+sub readDeviceIdDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->ReadDeviceID();
+	$cui->dialog(
+		-title => "Device ID",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
+
+sub versionInfoDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->VersionInfo();
+	$cui->dialog(
+		-title => "Device Version",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
+
+sub displayMessageDialog {
+	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	%reply = $FD->DisplayMessage("Hallo mitso");
+	$cui->dialog(
+		-title => "Device Message",
+		-message => sprintf("[%s]    ", $reply{DATA})
+	);
+}
