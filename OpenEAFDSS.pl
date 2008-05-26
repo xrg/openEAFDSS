f74c6a4e11e549607e250c1a91b17fd19b0b7f07
hasiotis <hasiotis@7a9393eb-3c4c-0410-9195-e5c8997374a0>
2008-05-26 11:54:57 +0000

    Console GUI again
    git-svn-id: http://openeafdss.googlecode.com/svn/trunk@10 7a9393eb-3c4c-0410-9195-e5c8997374a0

--- OpenEAFDSS.pl
+++ OpenEAFDSS.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl -w
 
+# Copyright (C) 2008 by Hasiotis Nikos
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use lib "EAFDSS/lib";
 use strict;
 use Config::IniFiles;
@@ -23,31 +38,37 @@ my($cui) = new Curses::UI(
 		);
 
 my($menuFile) = [
-	{ -label => 'Settings   ^S', -value => \&settingsDialog  },
-	{ -label => 'Exit       ^Q', -value => \&exitDialog  }
+	{ -label => ' Settings  ^S', -value => \&settingsDialog },
+	{ -label => ' Exit      ^Q', -value => \&exitDialog     }
 ];
 my($menuActions) = [
-	{ -label => 'Get Status        ^U', -value => \&getStatusDialog  },
-	{ -label => 'Set Headers       ^E', -value => \&setHeadersDialog  },
-	{ -label => 'Get Headers       ^R', -value => \&getHeadersDialog  },
-	{ -label => 'Read Time         ^T', -value => \&readTimeDialog  },
-	{ -label => 'Read Device ID    ^I', -value => \&readDeviceIdDialog  },
-	{ -label => 'Version Info      ^V', -value => \&versionInfoDialog  },
-	{ -label => 'Display Message   ^G', -value => \&displayMessageDialog  },
+	{ -label => ' Get Status     ', -value => \&getStatusDialog      },
+	{ -label => ' Set Headers    ', -value => \&setHeadersDialog     },
+	{ -label => ' Get Headers    ', -value => \&getHeadersDialog     },
+	{ -label => ' Read Time      ', -value => \&readTimeDialog       },
+	{ -label => ' Read Device ID ', -value => \&readDeviceIdDialog   },
+	{ -label => ' Version Info   ', -value => \&versionInfoDialog    },
+	{ -label => ' Display Message', -value => \&displayMessageDialog },
 ];
 my($menuUtilities) = [
-	{ -label => 'Validate  ^V', -value => \&validateDialog  }
+	{ -label => ' Browse Files   ', -value => \&browseDialog   },
+	{ -label => ' Validate Files ', -value => \&validateDialog },
+	{ -label => ' Check Device   ', -value => \&checkDialog    }
+];
+my($menuHelp) = [
+	{ -label => ' Help ', -value => \&helpDialog },
+	{ -label => ' About', -value => \&aboutDialog }
 ];
 my($menuBar) = [
-	{ -label => 'File',      -submenu => $menuFile },
-	{ -label => 'Actions',   -submenu => $menuActions },
-	{ -label => 'Utilities', -submenu => $menuUtilities}
+	{ -label => 'File',      -submenu => $menuFile      },
+	{ -label => 'Actions',   -submenu => $menuActions   },
+	{ -label => 'Utilities', -submenu => $menuUtilities },
+	{ -label => 'Help',      -submenu => $menuHelp      }
 ]; 
 
 my($menu) = $cui->add( 'menu', 'Menubar', -menu => $menuBar);
 
 my($statusBar) = $cui->add( 'statusbar_win', 'Window', -height => 4, -y => -1);
-
 my($status) = $statusBar->add(
 	'status_text', 'TextViewer',
 	-text		=> " ^X:Menu | OpenEAFDSS Demo Utility",
@@ -60,13 +81,6 @@ my($status) = $statusBar->add(
 $cui->set_binding(sub {$menu->focus()},     "\cX");
 $cui->set_binding( \&settingsDialog,        "\cS");
 $cui->set_binding( \&exitDialog,            "\cQ");
-#$cui->set_binding( \&getStatusDialog,       "\cU");
-#$cui->set_binding( \&setHeadersDialog,      "\cE");
-#$cui->set_binding( \&getHeadersDialog,      "\cR");
-#$cui->set_binding( \&readTimeDialog ,       "\cT");
-#$cui->set_binding( \&readDeviceIdDialog ,   "\cI");
-#$cui->set_binding( \&versionInfoDialog ,    "\cV");
-#$cui->set_binding( \&displayMessageDialog , "\cG");
 
 $cui->mainloop();
 
@@ -97,7 +111,6 @@ sub settingsDialog {
 		-padright       => 2,
 		-ipad           => 1
 	);
-
 
 	my($lblDeviceID) = $winSettings->add(
 		"lDeviceID", "Label", -text   => "    Device ID: ",
@@ -204,9 +217,8 @@ sub settingsDialog {
 	$winSettings->modalfocus();
 }
 
-
 sub getStatusDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->GetStatus();
 	$cui->dialog(
 		-title => "Device Status",
@@ -215,16 +227,113 @@ sub getStatusDialog {
 }
 
 sub setHeadersDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
-	%reply = $FD->SetHeader();
-	$cui->dialog(
-		-title => "Set Headers",
-		-message => sprintf("[%s]    ", $reply{DATA})
+	my($winSetHeaders) = $cui->add(
+		'winSetHeaders', 'Window',
+		-width          => 60,
+		-height         => 23,
+		-border         => 1,
+		-padtop         => 2,
+		-padbottom      => 2,
+		-padleft        => 2,
+		-padright       => 2,
+		-ipad           => 1
 	);
+
+	my($lblHeader01) = $winSetHeaders->add(
+		"lHeader01", "Label", -text   => "    Header Line #1: ",
+		-x      => 2, -y      => 1,
+		-height => 1, -width  => 20,
+		-maxlength => 11, -textalignment => 'right',
+	);
+	my($txtHeader01) = $winSetHeaders->add(
+		"txtHeader01", "TextEntry", -text   => "",
+		-fg     => 'black', -bg     => 'cyan',
+		-x      => 25, -y      => 1,
+		-height => 1, -width  => 12,
+		-maxlength => 11,
+	);
+
+	my($lblHeader02) = $winSetHeaders->add(
+		"lHeader02", "Label", -text   => "    Header Line #2: ",
+		-x      => 2, -y      => 2,
+		-height => 1, -width  => 20,
+		-maxlength => 11, -textalignment => 'right',
+	);
+	my($txtHeader02) = $winSetHeaders->add(
+		"txtHeader02", "TextEntry", -text   => "",
+		-fg     => 'black', -bg     => 'cyan',
+		-x      => 25, -y      => 2,
+		-height => 1, -width  => 12,
+		-maxlength => 11,
+	);
+
+	my($lblHeader03) = $winSetHeaders->add(
+		"lHeader03", "Label", -text   => "    Header Line #3: ",
+		-x      => 2, -y      => 3,
+		-height => 1, -width  => 20,
+		-maxlength => 11, -textalignment => 'right',
+	);
+	my($txtHeader03) = $winSetHeaders->add(
+		"txtHeader03", "TextEntry", -text   => "",
+		-fg     => 'black', -bg     => 'cyan',
+		-x      => 25, -y      => 3,
+		-height => 1, -width  => 12,
+		-maxlength => 11,
+	);
+
+	my($lblHeader04) = $winSetHeaders->add(
+		"lHeader04", "Label", -text   => "    Header Line #4: ",
+		-x      => 2, -y      => 4,
+		-height => 1, -width  => 20,
+		-maxlength => 11, -textalignment => 'right',
+	);
+	my($txtHeader04) = $winSetHeaders->add(
+		"txtHeader04", "TextEntry", -text   => "",
+		-fg     => 'black', -bg     => 'cyan',
+		-x      => 25, -y      => 4,
+		-height => 1, -width  => 12,
+		-maxlength => 11,
+	);
+
+	my($setHeadersCancel) = sub {
+		$winSetHeaders->loose_focus();
+		$cui->delete('winSetHeaders');
+	};
+
+	my($setHeadersOK) = sub {
+		$winSetHeaders->loose_focus();
+		$cui->delete('winSetHeaders');
+
+		my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
+		%reply = $FD->SetHeader();
+		$cui->dialog(
+			-title => "Set Headers",
+			-message => sprintf("[%s]    ", $reply{DATA})
+		);
+	};
+
+	my($btnBox) = $winSetHeaders->add(
+		"btnBox", "Buttonbox" ,
+		-y => -1,
+		-buttons => [
+			{ -label    => '< OK >',
+			  -shortcut => 'o',
+			  -value    => 1,
+			  -onpress  => $setHeadersOK},
+			{ -label    => '< Cancel >',
+			  -shortcut => 'c',
+			  -value    => 0,
+			  -onpress  => $setHeadersCancel}
+		],
+		-buttonalignment => 'middle'
+	);
+
+	$btnBox->focus();
+	$winSetHeaders->modalfocus();
 }
 
 sub getHeadersDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->GetHeader();
 	$cui->dialog(
 		-title => "Get Headers",
@@ -233,7 +342,7 @@ sub getHeadersDialog {
 }
 
 sub readTimeDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->ReadTime();
 	$cui->dialog(
 		-title => "Device Time",
@@ -242,7 +351,7 @@ sub readTimeDialog {
 }
 
 sub readDeviceIdDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->ReadDeviceID();
 	$cui->dialog(
 		-title => "Device ID",
@@ -251,7 +360,7 @@ sub readDeviceIdDialog {
 }
 
 sub versionInfoDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->VersionInfo();
 	$cui->dialog(
 		-title => "Device Version",
@@ -260,7 +369,7 @@ sub versionInfoDialog {
 }
 
 sub displayMessageDialog {
-	my($FD) = new EAFDSS::SDNP(DIR => "/tmp/SIGNS", SN => "ABC02000001", IP => "miles");
+	my($FD) = new EAFDSS::SDNP(DIR => $curSignsDir, SN => $curDeviceID, IP => $curIpAddress);
 	%reply = $FD->DisplayMessage("Hallo mitso");
 	$cui->dialog(
 		-title => "Device Message",
