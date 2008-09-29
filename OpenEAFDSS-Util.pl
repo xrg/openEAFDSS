fa0f01e12551903547e85a80b59d5940e3c8a59b
hasiotis <hasiotis@7a9393eb-3c4c-0410-9195-e5c8997374a0>
2008-09-29 09:12:08 +0000

    Preparing Command line utility
    git-svn-id: http://openeafdss.googlecode.com/svn/trunk@28 7a9393eb-3c4c-0410-9195-e5c8997374a0

--- OpenEAFDSS-Util.pl
+++ OpenEAFDSS-Util.pl
@@ -16,33 +16,40 @@ my(%progie) = ( name      => 'OpenEAFDSS-Util.pl',
 sub main() {
         my($driver, $params, $serial, $sDir, $cmd, $file) = init_progie();
 
-	my($fd) = new EAFDSS($driver . "::" . $params, $serial, $sDir);
+	#my($fd) = new EAFDSS($driver . "::" . $params, $serial, $sDir);
+	#if ($fd) {
+	#} else {
+	#	printf("Could not initialize signature device...\n");
+	#}
 	
-	#my($fd) = new EAFDSS("EAFDSS::SDNP::gattaca-wlan,24830", "ABC02000001", "/tmp/signs");
-	#my($fd) = new EAFDSS("EAFDSS::SDSP::/dev/ttyS0,38400", "ABC02000001", "/tmp/signs");
-	#my($fd) = new EAFDSS("EAFDSS::Dummy::/tmp/dummy-sd", "ABC02000001", "/tmp/signs");
-
-	my($reply, $sign) = $fd->Sign("invoice.txt");
-	printf("    SIGN --> [%s]\n", $sign);
 }
 
 sub init_progie() {
-        my(%opt, $valid, $cfg, $cmd, $driver, $serial, $params, $sDir, $file);
+        my(%opt, $valid, $cfg, $name, $cmd, $driver, $serial, $params, $sDir, $file);
         getopts('hvn:d:s:p:i:e:f:c:', \%opt);
 
         if ($opt{c}) {$cfg    = $opt{i}}  else {$cfg = "OpenEAFDSS-Util.conf"}
-	my(%cfg) = ParseConfig($cfg);
-
-	print Dumper $cfg{'LAN'};
-	exit;
+	my(%cfg) = ParseConfig(-ConfigFile => $cfg, -LowerCaseNames => 1);
 
         if ($opt{h}) {$valid  = "FALSE"}  else {$valid  = "TRUE"};
         if ($opt{v}) {$verbal = "TRUE"}   else {$verbal = "FALSE"};
-        if ($opt{e}) {$cmd    = $opt{e}}   else {$valid = "FALSE"};
+
+        if ($opt{n}) {$name   = $opt{n}}  else {$valid = "FALSE"};
+	if ($valid ne "FALSE") {
+		$name = lc($name);
+		$driver = $cfg{$name}{"driver"};
+		$serial = $cfg{$name}{"sn"};
+		$params = $cfg{$name}{"parameters"};
+	        $sDir   = $cfg{$name}{"dir"};
+	}
+
+        if ($opt{e}) {$cmd    = $opt{e}}  else {$valid = "FALSE"};
+
         if ($opt{d}) {$driver = $opt{d}};
         if ($opt{s}) {$serial = $opt{s}};
         if ($opt{p}) {$params = $opt{p}};
         if ($opt{i}) {$sDir   = $opt{i}};
+
         if ($opt{f}) {$file   = $opt{f}};
 
         if ($valid =~ /FALSE/) {
@@ -66,9 +73,12 @@ sub print_help() {
 	printf("\t  -e                 [command to execute (SIGN, DATE, TIME, STATUS)]\n");
 	printf("\t  -f                 [file to sign]\n");
 	printf("\t  -c <config file>   [which config file to use (default: OpenEAFDSS-Util.conf)]\n");
-	printf("\n  Example: $progie{name} -d EAFDSS::SDNP -p gattaca-wlan -e SIGN -f invoice.txt\n");
-	printf("\n           This command will sign the file invoice.txt printing the signature");
-	printf("\n           on the stdout.\n\n");
+	printf("\n  Example 1: $progie{name} -d EAFDSS::SDNP -p hostname -e SIGN -f invoice.txt\n");
+	printf("\n             This command will sign the file invoice.txt printing the signature");
+	printf("\n             on the stdout.\n\n");
+	printf("\n  Example 2: $progie{name} -n DEV-NAME -e STATUS\n");
+	printf("\n             This command will print the status of the device marked by DEV-NAME");
+	printf("\n             on the configuration file.\n\n");
 }
 
 main();
